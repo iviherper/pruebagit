02989d3c2c4ede30e0a46b8c08f47fdc8a625ace
Iván <iherper97@hotmail.com>
2020-01-18 23:27:16 +0100

    Funcionalidad tlf1 ok

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,6 +1,9 @@
 
 public class Vehiculo {
-	public static void main(String[] args) {
+	public static void arrancar() {
 		System.out.println("Brrrrr...acaba de arrancar!");
+	}
+	public static void main(String[] args) {
+		arrancar();
 	}
 }
